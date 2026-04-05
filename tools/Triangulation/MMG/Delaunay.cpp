1bf0f70cc90631fdf969c7a8cf805493a1e9be1c
erhannis <eyeillus@gmail.com>
2026-04-04 23:55:37 -0400

    Possible fix for upgrade break.

--- tools/Triangulation/MMG/Delaunay.cpp
+++ tools/Triangulation/MMG/Delaunay.cpp
@@ -42,7 +42,7 @@ void MMG::Delaunay::run() {
 
     const size_t num_points = m_points.rows();
     const size_t num_segments = m_segments.rows();
-    if (MMG2D_Set_meshSize(mmgMesh,num_points,0,num_segments) != 1) {
+    if (MMG2D_Set_meshSize(mmgMesh,num_points,0,0,num_segments) != 1) {
         throw RuntimeError("MMG2D_Set_meshSize failed.");
     }
 
@@ -81,9 +81,10 @@ void MMG::Delaunay::run() {
             throw NotImplementedError("Unknown MMG failure encountered.");
     }
 
-    int num_vertices, num_triangles, num_edges;
+    //CHECK Do we need to resize quads?
+    int num_vertices, num_triangles, num_quads, num_edges;
     if (MMG2D_Get_meshSize(mmgMesh,
-                &num_vertices, &num_triangles, &num_edges) !=1) {
+                &num_vertices, &num_triangles, &num_quads, &num_edges) !=1) {
         throw RuntimeError("MMG2D_Get_meshSize failed.");
     }
 
@@ -153,7 +154,7 @@ void MMG::Delaunay::refine(const MatrixFr& metric) {
         throw RuntimeError("Setting MMG2D_DPARAM_hmax failed.");
     }
 
-    if (MMG2D_Set_meshSize(mmgMesh,num_input_vertices,num_input_faces,0) != 1) {
+    if (MMG2D_Set_meshSize(mmgMesh,num_input_vertices,num_input_faces,0,0) != 1) {
         throw RuntimeError("MMG2D_Set_meshSize failed.");
     }
 
@@ -211,9 +212,10 @@ void MMG::Delaunay::refine(const MatrixFr& metric) {
             throw NotImplementedError("Unknown MMG failure encountered.");
     }
 
-    int num_vertices, num_triangles, num_edges;
+    //CHECK Do we need to resize quads?
+    int num_vertices, num_triangles, num_quads, num_edges;
     if (MMG2D_Get_meshSize(mmgMesh,
-                &num_vertices, &num_triangles, &num_edges) !=1) {
+                &num_vertices, &num_triangles, &num_quads, &num_edges) !=1) {
         throw RuntimeError("MMG2D_Get_meshSize failed.");
     }
 
