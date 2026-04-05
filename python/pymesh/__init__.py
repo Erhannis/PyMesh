835d141e5c1e69dc45a90ada5ee2092b863d256b
erhannis <eyeillus@gmail.com>
2026-04-05 01:11:35 -0400

    Dep change break

--- python/pymesh/__init__.py
+++ python/pymesh/__init__.py
@@ -2,8 +2,8 @@ from .version import __version__
 from . import PyMeshSetting
 from .timethis import timethis
 
-from numpy.testing import Tester
-test = Tester().test
+import numpy as np;
+np.test()
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
